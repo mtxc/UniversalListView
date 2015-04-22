4474dd2323022759e3f1f555a04b8db16568a6f3
mtxc <710649878@qq.com>
2015-04-22 22:02:20 +0800

    create project

---
+++ src/com/mtxc/universallistview/demo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.mtxc.universallistview.demo;
+
+import com.mtxc.universallistview.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
