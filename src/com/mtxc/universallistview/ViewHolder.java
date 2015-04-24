13ff638e737a6eb96659f6135bf6f2974d7ef725
mtxc <710649878@qq.com>
2015-04-24 12:52:00 +0800

    create ViewHolder

---
+++ src/com/mtxc/universallistview/ViewHolder.java
@@ -0,0 +1,96 @@
+package com.mtxc.universallistview;
+
+import android.content.Context;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ViewHolder {
+
+	/**
+	 * 存储item中的所有视图控件
+	 */
+	private SparseArray<View> mViews;
+
+	/**
+	 * 存储item所在位置
+	 */
+	private int mPosition;
+
+	/**
+	 * 存储convertView
+	 */
+	private View mConvertView;
+
+	/**
+	 * ViewHolder的构造方法
+	 * 
+	 * @param context
+	 *            传入上下文
+	 * @param position
+	 *            item所在位置
+	 * @param itemLayoutId
+	 *            item的布局文件
+	 * @param parent
+	 *            父控件
+	 */
+	public ViewHolder(Context context, int position, int itemLayoutId,
+			ViewGroup parent) {
+		this.mPosition = position;
+		this.mViews = new SparseArray<View>();
+		mConvertView = LayoutInflater.from(context).inflate(itemLayoutId,
+				parent, false);
+		mConvertView.setTag(this);
+	}
+
+	/**
+	 * 获取ViewHolder的入口方法
+	 * 
+	 * @param context
+	 *            传入上下文
+	 * @param position
+	 *            item所在位置
+	 * @param convertView
+	 *            adapter传入的convertView
+	 * @param itemLayoutId
+	 *            item的布局文件
+	 * @param parent
+	 *            父控件
+	 * @return
+	 */
+	public static ViewHolder get(Context context, int position,
+			View convertView, int itemLayoutId, ViewGroup parent) {
+		ViewHolder holder = null;
+		if (convertView == null) {
+			holder = new ViewHolder(context, position, itemLayoutId, parent);
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+			holder.mPosition = position;
+		}
+		return holder;
+	}
+
+	/**
+	 * 根据id获取item中的控件
+	 * @param viewId   控件id
+	 * @return    控件对象
+	 */
+	public <T extends View> T getView(int viewId) {
+		View view = mViews.get(viewId);
+		if (view == null) {
+			view = mConvertView.findViewById(viewId);
+			mViews.put(viewId, view);
+		}
+		return (T) view;
+	}
+
+	/**
+	 * 用于外界访问convertView
+	 * 
+	 * @return
+	 */
+	public View getConvertView() {
+		return mConvertView;
+	}
+}
--- src/com/mtxc/universallistview/demo/MyAdapter.java
+++ src/com/mtxc/universallistview/demo/MyAdapter.java
@@ -1,7 +1,10 @@
 package com.mtxc.universallistview.demo;
 
 import java.util.List;
+
 import com.mtxc.universallistview.R;
+import com.mtxc.universallistview.ViewHolder;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +16,7 @@ import android.widget.TextView;
 public class MyAdapter extends BaseAdapter {
 
 	private Context context;
-
+	
 	private LayoutInflater inflater;
 
 	private List<Data> datas;
@@ -41,48 +44,14 @@ public class MyAdapter extends BaseAdapter {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		ViewHolder holder;
-		if (convertView == null) {
-			convertView = inflater.inflate(R.layout.list_item, null);
-			holder = new ViewHolder();
-			holder.headImage = (ImageView) convertView
-					.findViewById(R.id.iv_head);
-			holder.tvName = (TextView) convertView.findViewById(R.id.tv_name);
-			holder.tvBlog = (TextView) convertView.findViewById(R.id.tv_blog);
-			holder.tvPraise = (TextView) convertView
-					.findViewById(R.id.tv_praise);
-			holder.tvBrowse = (TextView) convertView
-					.findViewById(R.id.tv_browse);
-			holder.tvComment = (TextView) convertView
-					.findViewById(R.id.tv_comment);
-			convertView.setTag(holder);
-		} else {
-			holder = (ViewHolder) convertView.getTag();
-		}
-		holder.headImage.setImageResource(datas.get(position).getHeadImage());
-		holder.tvName.setText(datas.get(position).getName());
-		holder.tvBlog.setText(datas.get(position).getBlog());
-		holder.tvPraise.setText("‘ﬁ(" + datas.get(position).getPraise() + ")");
-		holder.tvBrowse.setText("‰Ø¿¿(" + datas.get(position).getBrowse() + ")");
-		holder.tvComment
-				.setText("∆¿¬€(" + datas.get(position).getComment() + ")");
-		return convertView;
-	}
-
-	private class ViewHolder {
-
-		public ImageView headImage;
-
-		public TextView tvName;
-
-		public TextView tvBlog;
-
-		public TextView tvPraise;
-
-		public TextView tvBrowse;
-
-		public TextView tvComment;
-
+		ViewHolder holder = ViewHolder.get(context, position, convertView, R.layout.list_item, parent);
+		((ImageView)holder.getView(R.id.iv_head)).setImageResource(datas.get(position).getHeadImage());
+		((TextView)holder.getView(R.id.tv_name)).setText(datas.get(position).getName());
+		((TextView)holder.getView(R.id.tv_blog)).setText(datas.get(position).getBlog());
+		((TextView)holder.getView(R.id.tv_praise)).setText("‘ﬁ(" + datas.get(position).getPraise() + ")");
+		((TextView)holder.getView(R.id.tv_browse)).setText("‰Ø¿¿(" + datas.get(position).getBrowse() + ")");
+		((TextView)holder.getView(R.id.tv_comment)).setText("∆¿¬€(" + datas.get(position).getComment() + ")");
+		return holder.getConvertView();
 	}
 
 }
